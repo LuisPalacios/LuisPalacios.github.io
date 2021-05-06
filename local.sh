c90e669375fd5a651565a3ac559ce6f333f6064f
Luis Palacios <luis.palacios.derqui@gmail.com>
2021-05-06 17:52:15 +0200

    Implemento utteranc.es  (#4)

--- local.sh
+++ local.sh
@@ -2,4 +2,4 @@
 #
 # Script to run jekyll and to serve the pages locally 
 #
-bundle exec jekyll serve --host idefix.parchis.org --port 4000
+JEKYLL_ENV=development bundle exec jekyll serve --host idefix.parchis.org --port 4000
